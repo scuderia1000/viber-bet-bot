8528ae3c57567f992ebf62e5d155a9678829ba9a
scuderia1000 <a4cbepk@gmail.com>
2020-12-08 06:34:30 +0500

    add vercel url

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,9 @@ if (URL) {
 
   http
     .createServer(bot.middleware())
-    .listen(port, () => bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL));
+    .listen(port, () =>
+      bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL || process.env.VERCEL_URL),
+    );
 } else {
   logger.debug(
     'Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide.',
