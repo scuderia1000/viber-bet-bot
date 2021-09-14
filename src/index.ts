3a823e41a361d05bf815aa3691eeb4a6ddfd32f5
Valek <a4cbepk@gmail.com>
2021-09-14 09:23:49 +0500

    master - правки для сборки в heroku

--- src/index.ts
+++ src/index.ts
@@ -20,30 +20,38 @@ const HOST = '0.0.0.0';
 logger.debug('URL: %s', URL);
 logger.debug('PORT: %s', PORT);
 
-connectDb()
-  .then((db) => {
-    const modules = getModules(db);
-    configSchedulers(modules);
-    const bot = initializeBot(TOKEN, modules);
-
-    if (URL) {
-      https.createServer(bot.middleware()).listen(PORT, HOST, () => bot.setWebhook(URL));
-    } else {
-      logger.debug(
-        'Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide.',
-      );
-      ngrok
-        .getPublicUrl()
-        .then((publicUrl) => {
-          http.createServer(bot.middleware()).listen(PORT, () => bot.setWebhook(publicUrl));
-        })
-        .catch((error) => {
-          logger.debug('Can not connectDb to ngrok server. Is it running?');
-          logger.error(error);
-          process.exit(1);
-        });
-    }
+http
+  .createServer((request, response) => {
+    console.log('request', request);
   })
-  .catch((err) => {
-    logger.error(`DB error: `, err);
+  .listen(PORT, HOST, () => {
+    console.log('listen');
   });
+
+// connectDb()
+//   .then((db) => {
+//     const modules = getModules(db);
+//     configSchedulers(modules);
+//     const bot = initializeBot(TOKEN, modules);
+//
+//     if (URL) {
+//       https.createServer(bot.middleware()).listen(PORT, HOST, () => bot.setWebhook(URL));
+//     } else {
+//       logger.debug(
+//         'Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide.',
+//       );
+//       ngrok
+//         .getPublicUrl()
+//         .then((publicUrl) => {
+//           http.createServer(bot.middleware()).listen(PORT, () => bot.setWebhook(publicUrl));
+//         })
+//         .catch((error) => {
+//           logger.debug('Can not connectDb to ngrok server. Is it running?');
+//           logger.error(error);
+//           process.exit(1);
+//         });
+//     }
+//   })
+//   .catch((err) => {
+//     logger.error(`DB error: `, err);
+//   });
